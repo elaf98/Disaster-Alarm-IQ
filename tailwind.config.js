ef1a2ec2ac19c7c0ae9134deb62f99e25319edc9
hank51111 <jonewhaterson@gmail.com>
2025-12-14 18:43:29 +0300

    update vite to 7.2.7

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     extend: {
       colors: {
         primary: '#0f4c75',
-        secondary: '#03f785ff',
+        secondary: '#0aff3fff',
         accent: '#1b262c',
         danger: '#c81912',
         warning: '#ff9a3c',
