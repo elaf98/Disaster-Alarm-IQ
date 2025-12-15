ce2c41d6012083ee55f1bf43cc9ba74bbd5de377
hank51111 <jonewhaterson@gmail.com>
2025-12-15 20:25:55 +0300

    map update 0.5

--- src/components/DashboardMap.jsx
+++ src/components/DashboardMap.jsx
@@ -21,14 +21,126 @@ const DashboardMap = () => {
   const disasters = [
     {
       id: 1,
-      position: [33.3152, 44.3661], // Baghdad
+      position: [37.10726731554552, 43.517877960398145], // دهوك
+      type: 'critical',
+      title: 'Sandstorm Alert',
+      description: 'Severe sandstorm expected in Baghdad area'
+    },
+    {
+      id: 2,
+      position: [36.47503862984028, 43.176506528801234], // الموصل
       type: 'critical',
       title: 'Sandstorm Alert',
       description: 'Severe sandstorm expected in Baghdad area'
     },
     {
       id: 3,
-      position: [30.5, 47.8], // Basra
+      position: [35.49646663095943, 42.15746981111663], // نينوى
+      type: 'critical',
+      title: 'Sandstorm Alert',
+      description: 'Severe sandstorm expected in Baghdad area'
+    },
+    {
+      id: 4,
+      position: [35.69816327337555, 45.234275335558664], // سليمانية
+      type: 'critical',
+      title: 'Sandstorm Alert',
+      description: 'Severe sandstorm expected in Baghdad area'
+    },
+    {
+      id: 5,
+      position: [35.60445674173643, 44.421898652666265], // كركوك
+      type: 'critical',
+      title: 'Sandstorm Alert',
+      description: 'Severe sandstorm expected in Baghdad area'
+    },
+    {
+      id: 6,
+      position: [34.28506257801299, 43.91491321386586], // صلاح الدين
+      type: 'critical',
+      title: 'Sandstorm Alert',
+      description: 'Severe sandstorm expected in Baghdad area'
+    },
+    {
+      id: 7,
+      position: [34.23539261101675, 45.05779965015749], // ديالى
+      type: 'critical',
+      title: 'Sandstorm Alert',
+      description: 'Severe sandstorm expected in Baghdad area'
+    },
+    {
+      id: 8,
+      position: [33.3152, 44.3661], // بغداد
+      type: 'critical',
+      title: 'Sandstorm Alert',
+      description: 'Severe sandstorm expected in Baghdad area'
+    },
+    {
+      id: 9,
+      position: [32.66191500321363, 45.807644201929975], // واسط
+      type: 'advisory',
+      title: 'Flood Warning',
+      description: 'Heavy rainfall expected in southern regions'
+    },
+    {
+      id: 10,
+      position: [33.34124483059142, 40.79067680034809], // الأنبار
+      type: 'advisory',
+      title: 'Flood Warning',
+      description: 'Heavy rainfall expected in southern regions'
+    },
+    {
+      id: 11,
+      position: [32.59589637454824, 43.95241141836148], // كربلاء
+      type: 'advisory',
+      title: 'Flood Warning',
+      description: 'Heavy rainfall expected in southern regions'
+    },
+    {
+      id: 12,
+      position: [32.59362162683641, 44.96627213805818], // بابل
+      type: 'advisory',
+      title: 'Flood Warning',
+      description: 'Heavy rainfall expected in southern regions'
+    },
+    {
+      id: 13,
+      position: [31.822096348666292, 44.10919163933003], // النجف
+      type: 'advisory',
+      title: 'Flood Warning',
+      description: 'Heavy rainfall expected in southern regions'
+    },
+    {
+      id: 14,
+      position: [32.02796970608617, 44.88142262116236], // الديوانية
+      type: 'advisory',
+      title: 'Flood Warning',
+      description: 'Heavy rainfall expected in southern regions'
+    },
+    {
+      id: 15,
+      position: [30.39089124210332, 45.544056549928285], // المثنى
+      type: 'advisory',
+      title: 'Flood Warning',
+      description: 'Heavy rainfall expected in southern regions'
+    },
+    {
+      id: 16,
+      position: [31.069488654774624, 46.165009339418596], // ذي قار
+      type: 'advisory',
+      title: 'Flood Warning',
+      description: 'Heavy rainfall expected in southern regions'
+    },
+    {
+      id: 17,
+      position: [31.957831674723298, 47.167797756055315], // ميسان
+      type: 'advisory',
+      title: 'Flood Warning',
+      description: 'Heavy rainfall expected in southern regions'
+    },
+    {
+      id: 18,
+      position: [30.5, 47.8], // البصرة
       type: 'advisory',
       title: 'Flood Warning',
       description: 'Heavy rainfall expected in southern regions'
